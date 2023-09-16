479eb756019cd7f6be2f0101a89bd945c692457b
HarshalRajnoor <harshalrajnor852056@gmail.com>
2023-09-16 16:35:15 +0530

    modified login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,24 @@ import "./login.scss"
 
 const Login = () => {
   return (
-    <div className="login">Login</div>
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Edumo</h1>
+          <p>edumo is a web application that empowers users to showcase their academic and technical accomplishments.</p>
+          <span>Don't you have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form >
+            <input type="text" placeholder="Username"/>
+            <input type="password" placeholder="Password"/>
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
