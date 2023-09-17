e8ffcbd5390c01199e326385884a16a49d6d43d4
Harshal Rajnoor <harshalrajnor852056@gmail.com>
2023-09-17 11:39:16 +0530

    modified register page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
+
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
     <div>
-      <Login />
+      <Register />
     </div>
   );
 }
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
 import "./register.scss"
 
 const Register = () => {
-    return (
-        <div>Register</div>
-    )
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Edumo</h1>
+          <p>edumo is a web application that empowers users to showcase their academic and technical accomplishments.</p>
+          <span>Do you have an account?</span>
+          <button>Login</button>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form >
+            <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Register
