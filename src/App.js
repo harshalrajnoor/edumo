4d00f9df82f8c75850d34d3023f35224b16233d4
Harshal Rajnoor <harshalrajnor852056@gmail.com>
2023-09-19 08:51:50 +0530

    modified routes

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import {
   RouterProvider,
   Route,
   Outlet,
+  Navigate,
 } from "react-router-dom";
 
 import NavBar from "./components/navBar/NavBar";
 import LeftBar from "./components/leftBar/LeftBar";
 import RightBar from "./components/rightBar/RightBar";
-import Profile from "./pages/profile/Profile"
-import Home from "./pages/home/Home"
-
+import Profile from "./pages/profile/Profile";
+import Home from "./pages/home/Home";
 
 function App() {
+  const currentUser = false;
+
   const Layout = () => {
     return (
       <div>
@@ -28,10 +30,22 @@ function App() {
     );
   };
 
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
+
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout />,
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
       children: [
         {
           path: "/",
