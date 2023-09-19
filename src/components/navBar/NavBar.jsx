863020b4485ddb25b69c2a9506e6476ea6c58b0a
Harshal Rajnoor <harshalrajnor852056@gmail.com>
2023-09-19 10:29:10 +0530

    added navbar

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import "./navBar.scss"
+
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { Link } from 'react-router-dom';
+
+
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div className='navbar'>
+      <div className="left">
+        <Link to='/' style={{ textDecoration: "none" }}>
+          <span>Edumo</span>
+        </Link>
+
+        <HomeOutlinedIcon />
+        <DarkModeOutlinedIcon />
+        <GridViewOutlinedIcon />
+
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder='Search...' />
+        </div>
+
+      </div>
+
+      <div className="right">
+        <PersonOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <div className="user">
+          <img src="https://images.pexels.com/photos/15253379/pexels-photo-15253379/free-photo-of-off-road-car-standing-on-a-snowed-side-road.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+          <span>Harshal Rajnoor</span>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
